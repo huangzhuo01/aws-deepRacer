ccb2307c9b6ec4f1b3f4609e60fa76ea2886d45a
zhuxingjian <zhuxingjian@kingsoft.com>
2022-09-06 20:28:37 +0800

    add steering factor

--- mock_params.py
+++ mock_params.py
@@ -2,25 +2,24 @@ params ={
     "all_wheels_on_track": True,        # flag to indicate if the agent is on the track
     "x": 0.1,                            # agent's x-coordinate in meters
     "y": 0.1,                            # agent's y-coordinate in meters
-    "closest_objects": [0, 0],         # zero-based indices of the two closest objects to the agent's current position of (x, y).
-    "closest_waypoints": [0, 0],       # indices of the two nearest waypoints.
-    "distance_from_center": 0.1,         # distance in meters from the track center 
+    "closest_waypoints": [0, 1],       # indices of the two nearest waypoints.
+    "distance_from_center": 0.5,         # distance in meters from the track center 
     "is_crashed": False,                 # Boolean flag to indicate whether the agent has crashed.
     "is_left_of_center": False,          # Flag to indicate if the agent is on the left side to the track center or not. 
     "is_offtrack": False,                # Boolean flag to indicate whether the agent has gone off track.
     "is_reversed": False,                # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
-    "heading": 10,                      # agent's yaw in degrees
+    "heading": 45.0,                      # agent's yaw in degrees
     "objects_distance": [0.1, ],         # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
     "objects_heading": [0.1, ],          # list of the objects' headings in degrees between -180 and 180.
     "objects_left_of_center": [False, ], # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
     "objects_location": [(0.1, 0.1),], # list of object locations [(x,y), ...].
     "objects_speed": [0.1, ],            # list of the objects' speeds in meters per second.
     "progress": 0.1,                     # percentage of track completed
-    "speed": 0.1,                        # agent's speed in meters per second (m/s)
-    "steering_angle": 0.1,               # agent's steering angle in degrees
+    "speed": 4,                        # agent's speed in meters per second (m/s)
+    "steering_angle": 0.0,               # agent's steering angle in degrees
     "steps": 2,                          # number steps completed
     "track_length": 0.1,                 # track length in meters.
-    "track_width": 0.1,                  # width of the track
-    "waypoints": [(0.1, 0.1), ]        # list of (x,y) as milestones along the track center
+    "track_width": 1.0,                  # width of the track
+    "waypoints": [(1.0, 1.0), (2.0, 2.0), (3.0, 3.0), (4.0, 4.0), ]        # list of (x,y) as milestones along the track center
 
 }
--- zhuxingjian.py
+++ zhuxingjian.py
@@ -6,6 +6,12 @@ import math
 LIMITS_SPEED = [1, 4]
 LIMITS_STEERING_ANGLE = [-30, 30]
 
+def reduce(f, s):
+    r = 0
+    for i in s:
+        r = f(r, i)
+    return r
+
 def angle_of_vector(vector):
     rad = math.atan2(vector[1][1] - vector[0][1], vector[1][0] - vector[0][0])
     return math.degrees(rad)
@@ -14,11 +20,6 @@ def distance_of_vector(vector):
     return ((vector[1][1] - vector[0][1]) ** 2 + (vector[1][0] - vector[0][0]) ** 2) ** 0.5
 
 def span_of_points(points):
-    def reduce(f, s):
-        r = 0
-        for i in s:
-            r += i
-        return r
     return reduce(lambda x, y: x + y, [distance_of_vector([points[i], points[i+1]]) for i in range(len(points)-1)])
 
 def reward_function(params):
@@ -34,12 +35,12 @@ def reward_function(params):
     # is_left_of_center       = params["is_left_of_center"]
     steps                   = params["steps"]
     track_width             = params["track_width"]
-    # heading_direction       = params["heading"] # 航行方向
-    # track_direction         = angle_of_vector([waypoints[pre] waypoints[nxt]]) # 道路方向
-    # steering_angle          = params["steering_angle"]
-    # heading_angle           = abs(angle_of_vector([waypoints[pre] waypoints[nxt]])-params["heading"]) # 航向-道路夹角
-    forward_waypoints       = [waypoints[(pre+i)%len(waypoints)] for i in range(3)] # 前方航向关键点
-    # forward_directions      = [angle_of_vector([waypoints[(pre+i)%len(waypoints)] waypoints[(nxt+i)%len(waypoints)]]) for i in range(3)], # 前方航向弯度
+    heading_direction       = params["heading"] # 航行方向
+    # track_direction         = angle_of_vector([waypoints[pre], waypoints[nxt]]) # 道路方向
+    steering_angle          = params["steering_angle"]
+    # heading_angle           = abs(angle_of_vector([waypoints[pre], waypoints[nxt]])-params["heading"]) # 航向-道路夹角
+    forward_waypoints       = [waypoints[(nxt+i)%len(waypoints)] for i in range(3)] # 前方航向关键点
+    forward_directions      = [angle_of_vector([waypoints[(pre+i)%len(waypoints)], waypoints[(nxt+i)%len(waypoints)]]) for i in range(3)] # 前方航向弯度
 
     f = distance_of_vector([forward_waypoints[0], forward_waypoints[-1]]) / span_of_points(forward_waypoints)  # 前路弯曲因子 (0,1]越小说明越弯曲
     s = float(speed - LIMITS_SPEED[0]) / (LIMITS_SPEED[1] - LIMITS_SPEED[0]) # 速度因子 [0,1]
@@ -52,15 +53,30 @@ def reward_function(params):
         else:
             return math.sin(math.pi/2*s*1/f)
     else:
-        w = 9 * (1-float(distance_from_center)/track_width*2) + 1 # 偏移中心轴因子 [1,10]
-        s1 = 9 * s + 1 # 速度因子 [1,10]
+        B = 2
+        a = max(LIMITS_STEERING_ANGLE[0],min(LIMITS_STEERING_ANGLE[1],reduce(
+            lambda x, y: x + y,
+            [forward_directions[0]-heading_direction]+
+            [forward_directions[i+1]-forward_directions[i] for i in range(len(forward_directions)-1)]
+            )
+        ))
+        if a * steering_angle < 0:
+            a1 = 2*math.sin(math.pi/2*abs(a - steering_angle)/60)-1 # 转角因子,反向, [0,1]越小说明转角越大
+            if a1 < 0:
+                return a1
+            else:
+                a1 = B * a1 + 1
+        else:
+            a1 = B * (1.0-math.tan(abs(a - steering_angle)/30 * math.pi / 4)) + 1 # 转角因子,同向 [1,B+1]越小说明转角越小
+        w = B * (1.0-float(distance_from_center)/track_width*2) + 1 # 偏移中心轴因子 [1,B+1]
+        s1 = B * s + 1 # 速度因子 [1,B+1]
         t = 1.0
         if progress > 0:
-            tpp = float(steps) / progress
+            tpp = float(progress) / steps
             if hasattr(reward_function, 'steps'):
                 t = tpp / reward_function.steps # 完成度因子
             reward_function.steps = tpp
-        return s1 * w * t
+        return a1 * s1 * w * t
 
 if __name__ == "__main__":
     from mock_params import params
