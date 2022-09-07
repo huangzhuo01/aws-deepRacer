02ea4215bf6e546e9866316376cdead39c94fbfa
zhuxingjian <zhuxingjian@kingsoft.com>
2022-09-07 15:00:45 +0800

    zxj

--- zhuxingjian.py
+++ zhuxingjian.py
@@ -27,6 +27,10 @@ def diff_of_angle(angle1, angle2):
     r = angle1 - angle2
     return r if (r >= 0) else (360.0 + r)
 
+def sum_of_angle(angle1, angle2):
+    r = angle1 + angle2
+    return r if (r <= 360) else (r - 360.0)
+
 def reward_function(params):
     waypoints   = params["waypoints"]
     pre         = params['closest_waypoints'][0]
@@ -43,7 +47,7 @@ def reward_function(params):
     heading_direction       = params["heading"] # 航行方向
     # track_direction         = angle_of_vector([waypoints[pre], waypoints[nxt]]) # 道路方向
     steering_angle          = params["steering_angle"]
-    # heading_angle           = abs(diff_of_angle(angle_of_vector([waypoints[pre], waypoints[nxt]]),params["heading"])) # 航向-道路夹角
+    heading_angle           = diff_of_angle(params["heading"], angle_of_vector([waypoints[pre], waypoints[nxt]])) # 航向-道路夹角
     forward_waypoints       = [waypoints[(nxt+i)%len(waypoints)] for i in range(3)] # 前方航向关键点
     forward_directions      = [angle_of_vector([waypoints[(pre+i)%len(waypoints)], waypoints[(nxt+i)%len(waypoints)]]) for i in range(3)] # 前方航向弯度
 
@@ -60,19 +64,26 @@ def reward_function(params):
             return math.sin(math.pi/2*s*1/f)
     else:
         B = 8.0
-        a = max(LIMITS_STEERING_ANGLE[0],min(LIMITS_STEERING_ANGLE[1],reduce(
-            lambda x, y: x + y,
-            [diff_of_angle(forward_directions[0],heading_direction)]+
-            [diff_of_angle(forward_directions[i+1],forward_directions[i]) for i in range(len(forward_directions)-1)]
-        )))
-        if a * steering_angle < 0:
-            a1 = 2*math.sin(math.pi/2*abs(a - steering_angle)/LIMITS_STEERING_ANGLE[1]-LIMITS_STEERING_ANGLE[0])-1 # 转角因子,反向, [0,1]越小说明转角越大
-            if a1 < 0:
-                return a1
-            else:
-                a1 = B/2 * a1 + 1
-        else:
-            a1 = B/2 * (1.0-math.tan(abs(a - steering_angle)/abs(LIMITS_STEERING_ANGLE[0] if (a < 0 or steering_angle < 0) else LIMITS_STEERING_ANGLE[1]) * math.pi / 4)) + 1 # 转角因子,同向 [1,B+1]越小说明转角越小
+        # a = max(LIMITS_STEERING_ANGLE[0],min(LIMITS_STEERING_ANGLE[1],reduce(
+        #     lambda x, y: x + y,
+        #     [diff_of_angle(forward_directions[0],heading_direction)]+
+        #     [diff_of_angle(forward_directions[i+1],forward_directions[i]) for i in range(len(forward_directions)-1)]
+        # )))
+        # if a * steering_angle < 0:
+        #     a1 = 2*math.sin(math.pi/2*abs(a - steering_angle)/LIMITS_STEERING_ANGLE[1]-LIMITS_STEERING_ANGLE[0])-1 # 转角因子,反向, [0,1]越小说明转角越大
+        #     if a1 < 0:
+        #         return a1
+        #     else:
+        #         a1 = B/2 * a1 + 1
+        # else:
+        #     a1 = B/2 * (1.0-math.tan(abs(a - steering_angle)/abs(LIMITS_STEERING_ANGLE[0] if (a < 0 or steering_angle < 0) else LIMITS_STEERING_ANGLE[1]) * math.pi / 4)) + 1 # 转角因子,同向 [1,B+1]越小说明转角越小
+        a1 = 1.0
+        if reduce(lambda x, y: x + y, forward_directions) == forward_directions[0] * len(forward_directions): # 直道
+            a = 100*math.cos(math.pi/2*min(sum_of_angle(heading_angle, steering_angle), 90)/90)-99
+            if a < 0:
+                return a
+            a1 = B * a + 1
+
         w = B * (1.0-float(distance_from_center)/track_width*2) + 1 # 偏移中心轴因子 [1,B+1]
         s1 = B * s + 1 # 速度因子 [1,B+1]
         t = 1.0
@@ -82,7 +93,7 @@ def reward_function(params):
                 t = tpp / reward_function.steps # 完成度因子
             reward_function.steps = tpp
         rwd = a1 * s1 * w * t
-        print("R=%s (a1:%s, s1:%s, w:%s, t:%s) - is_offtrack:%s, speed:%s, progress:%s, all_wheels_on_track:%s, distance_from_center:%s, steps:%s, track_width:%s, heading_direction:%s, steering_angle:%s, forward_waypoints:%s, forward_directions:%s" % (rwd, a1, s1, w, t, is_offtrack,speed,progress,all_wheels_on_track,distance_from_center,steps,track_width,heading_direction,steering_angle,forward_waypoints,forward_directions))
+        print("R=%s (a1:%s, s1:%s, w:%s, t:%s) - is_offtrack:%s, speed:%s, progress:%s, all_wheels_on_track:%s, distance_from_center:%s, steps:%s, track_width:%s, heading_direction:%s, steering_angle:%s, heading_angle:%s, forward_waypoints:%s, forward_directions:%s" % (rwd, a1, s1, w, t, is_offtrack,speed,progress,all_wheels_on_track,distance_from_center,steps,track_width,heading_direction,steering_angle,heading_angle,forward_waypoints,forward_directions))
         return rwd
 
 if __name__ == "__main__":
